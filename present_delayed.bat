90a6c3dc9eba2aa8a7c425d1145f7f61589d552d
vinjn <vinjn.z@gmail.com>
2014-11-22 22:01:55 +0800

    Reorg

--- present_delayed.bat
+++ present_delayed.bat
@@ -1,2 +1,2 @@
 ping localhost -n 5
-processing-java --sketch=%~dp0\Sketch --output=%~dp0\build-tmp --force --present
+processing-java --sketch=%~dp0\Sketch --output=%~dp0\Sketch\build-tmp --force --present
